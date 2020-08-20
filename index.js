f167598cfc608e67db5201bfbc999be7b9741e4c
Ivan Zhilnikov <i.zhilnikov@bars.group>
2020-08-20 23:42:48 +0300

    добаление post

--- index.js
+++ index.js
@@ -6,28 +6,53 @@
 // eslint-disable-next-line no-alert, quotes, semi
 process.env.NODE_ENV = 'production';
 
-const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
-const Router = require('koa-router');
-const koaBody = require('koa-body');
-const cors = require('@koa/cors');
-const table = require('./tables');
-const moment = require('moment');
-const clc = require('cli-color');
+const Koa = require('koa'),
+	//подключение koa сервера
+	bodyParser = require('koa-bodyparser'),
+	//парсер параметров адресной строки
+	Router = require('koa-router'),
+	//подключени роутера
+	koaBody = require('koa-body'),
+	//подуль для получение параметров post
+	cors = require('@koa/cors'),
+	//подключение cors чтобы можно было делать запросы со сторонних ресурсов
+	table = require('./tables'),
+	//модуль с таблицами
+	moment = require('moment'),
+	//библиотека для рабты с дайтой и временем
+	clc = require('cli-color'),
+	//цвеитовая подсветка в консоли
+	app = new Koa(),
+	//создание экземплята сервера
+	router = new Router();
+//создание нового экземпляра роутера
 
-const app = new Koa();
-let router = new Router();
 app.use(cors());
+//установка cors для сервера
 app.use(bodyParser({ strict: false }));
+//устанока парсера параметров для сервера
 app.use(koaBody());
-
+//устанока парсера параметров для сервера POST
 app.use(router.routes()).use(router.allowedMethods());
+//установка роутера для сервера
 
-router.get('/auth/:username/:password', async (ctx, next) => {
+app.use(function(ctx) {
+  console.log(ctx)
+  ctx.body = { status: 'OK' };
+});
+
+/**
+ ** Получение данных авторизации 
+ * @method post
+ * @param {String} username Имя пользваотеля
+ * @param {String} pass Пароль пользователя
+  */
+router.post('/auth', async (ctx, next) => {
+  console.log('Авторизация')
 	//параметры запроса
-	const arg = ctx.params;
+ 	const arg = ctx.params;
 	console.log('arg', arg);
-	let USER;
+	/* let USER;
 	await Users.findOne({
 		where: {
 			username: arg.username,
@@ -36,8 +61,8 @@ router.get('/auth/:username/:password', async (ctx, next) => {
 		raw: true,
 	}).then((res) => {
 		USER = res;
-	});
-	ctx.body = USER;
+	}); */
+	ctx.body = {USER : 'test'} ;
 });
 
 /*
@@ -52,54 +77,50 @@ let keyDate = {
 	day: date.getDay(),
 };
 
-
 router.get('/', async (ctx, next) => {
 	ctx.body = { data: 'error' };
 });
 
-router.get('/getClientServer', async ctx => {
-  console.log('1231')
-  let data =await table.dataUser.findAll();
-  ctx.body = data;
-})
+router.get('/getClientServer', async (ctx) => {
+	console.log('1231');
+	let data = await table.dataUser.findAll();
+	ctx.body = data;
+});
 
 //полчение данных из 1с
-router.get(
-	`/getClient1C/${keyDate.month}${keyDate.date}/:data`,
-	async (ctx, next) => {
-		const arg = JSON.parse(ctx.params.data);
-		//console.log(clc.yellow('arg'), arg);
-		let checkNumCard = await table.dataUser.findOne({
-			where: {
-				numCard: Number(arg.numCard),
-			},
-		}); //првоерка наличия карты в базе
-		let obj = {
-			fio: arg.FIO,
-			email: arg.email,
-			phone: arg.phone,
-			numCard: arg.numCard,
-			SummaBonusa: arg.SummaBonusa,
-		};
-		if (arg.dr != '01.01.0001 0:00:00') {
-			let date = arg.dr.split(' ')[0];
-			date = moment(date, 'DD.MM.YYYY').format('YYYY-MM-DD');
-			obj.dr = date;
-		}
+router.get(`/getClient1C/${keyDate.month}${keyDate.date}/:data`, async (ctx, next) => {
+	const arg = JSON.parse(ctx.params.data);
+	//console.log(clc.yellow('arg'), arg);
+	let checkNumCard = await table.dataUser.findOne({
+		where: {
+			numCard: Number(arg.numCard),
+		},
+	}); //првоерка наличия карты в базе
+	let obj = {
+		fio: arg.FIO,
+		email: arg.email,
+		phone: arg.phone,
+		numCard: arg.numCard,
+		SummaBonusa: arg.SummaBonusa,
+	};
+	if (arg.dr != '01.01.0001 0:00:00') {
+		let date = arg.dr.split(' ')[0];
+		date = moment(date, 'DD.MM.YYYY').format('YYYY-MM-DD');
+		obj.dr = date;
+	}
 
-		//console.log('checkNumCard', checkNumCard)
-		if (!checkNumCard) {
-			table.dataUser.create(obj);
-		} else {
-			table.dataUser.update(obj, {
-				where: {
-					numCard: obj.numCard,
-				},
-			})
-		}
-		ctx.body = { data: 'error' };
-	},
-);
+	//console.log('checkNumCard', checkNumCard)
+	if (!checkNumCard) {
+		table.dataUser.create(obj);
+	} else {
+		table.dataUser.update(obj, {
+			where: {
+				numCard: obj.numCard,
+			},
+		});
+	}
+	ctx.body = { data: 'error' };
+});
 
 /********************
  **  Запуск сервера *
